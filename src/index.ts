d3579d8c9a1eec02ae2c98dd6c8e2119f14f3ddb
cristianceamatuassist <cristian.ceamatu@assist.ro>
2022-08-22 12:34:47 +0300

    Fix build bug that was bundling to ES6 and breaking aquarius tests, remove source mapping for production builds

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import CallService from './core/CallService';
 import Conversation from './core/Conversation';
 import Ieee from './audio/codecs/Ieee';
 import Mulaw from './audio/codecs/Mulaw';
-
+// wawww
 export { Base64, Conversation, Ieee, Mulaw }; // Potentially useful exports.
 export default CallService; // Probably what you want.
