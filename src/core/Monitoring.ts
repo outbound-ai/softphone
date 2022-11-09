01f12d70c0922c1b4b3e26b0c5f1681b5640a924
Ceamatu Cristian <cristian.ceamatu@assist.ro>
2022-11-09 22:16:41 +0200

    Basic latencyTest without a backend

---
+++ src/core/Monitoring.ts
@@ -0,0 +1,19 @@
+export default class Monitoring {
+  public initWebSocketLatencyTest(hostname: string, accessToken: string) {
+    const url = `${hostname}/api/v1/monitoring/browser`;
+
+    const webSocket = new WebSocket(url, ['access_token', accessToken]);
+    webSocket.addEventListener('open', () => console.log('Monitoring connection to the web socket opened'));
+
+    webSocket.addEventListener('message', (message: MessageEvent) => {
+      console.log('message', message);
+      webSocket.send(
+        JSON.stringify({
+          timestamp: new Date().toISOString()
+        })
+      );
+    });
+
+    webSocket.addEventListener('close', () => console.log('Monitoring connection to the web socket closed'));
+  }
+}
