a742125a6da1444361dce8bf663e9f0087bda536
Claude Andrew <claudea@outbound.ai>
2022-11-11 19:20:44 -0800

    Works with calls service OM-3102

--- src/core/Monitoring.ts
+++ src/core/Monitoring.ts
@@ -1,19 +1,43 @@
+interface IPingSessionMessage {
+    messageType: string
+}
+
+interface IPingSessionEchoMessage extends IPingSessionMessage {
+    echo?: string
+}
+
 export default class Monitoring {
-  static initWebSocketLatencyTest(hostname: string, accessToken: string) {
-    const url = `${hostname}/api/v1/monitoring/browser`;
+    static initWebSocketLatencyTest(hostname: string, accessToken: string) {
+        const url = `${hostname}/api/v1/pingresponder/some-id-for-this-device-or-user`;
 
-    const webSocket = new WebSocket(url, ['access_token', accessToken]);
-    webSocket.addEventListener('open', () => console.log('Monitoring connection to the web socket opened'));
+        const webSocket = new WebSocket(url, ['access_token', accessToken]);
 
-    webSocket.addEventListener('message', (message: MessageEvent) => {
-      console.log('message', message);
-      webSocket.send(
-        JSON.stringify({
-          timestamp: new Date().toISOString()
+        let sendPing: NodeJS.Timer | null;
+        webSocket.addEventListener('open', () => {
+            console.log('Monitoring connection to the web socket opened')
+            sendPing = setInterval(() => {
+                const echoMessage: IPingSessionEchoMessage = {
+                    messageType: 'echo',
+                    // might want to use something finer-grained than milliseconds.
+                    echo: new Date().toISOString(),
+                }
+                webSocket.send(JSON.stringify(echoMessage))
+            }, 5000)
         })
-      );
-    });
 
-    webSocket.addEventListener('close', () => console.log('Monitoring connection to the web socket closed'));
-  }
+        webSocket.addEventListener('message', (message: MessageEvent) => {
+            const nowDate = new Date()
+            const pingSessionMessage:IPingSessionEchoMessage = JSON.parse(message.data)
+            const thenString = pingSessionMessage.echo
+            const thenTime = Date.parse(thenString!)
+            const elapsedTime = nowDate.getTime()-thenTime
+            console.log(`RTT ${elapsedTime}ms`)
+        })
+
+        webSocket.addEventListener('close', () => {
+            console.log('Monitoring connection to the web socket closed')
+            if (sendPing)
+                clearInterval(sendPing)
+        });
+    }
 }
