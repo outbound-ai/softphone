d9ef0cf848e38ae7a39a0420985f0f452d80d34a
Ceamatu Cristian <cristian.ceamatu@assist.ro>
2022-11-18 00:54:19 +0200

    yb

--- src/core/Monitoring.ts
+++ src/core/Monitoring.ts
@@ -1,47 +1,53 @@
 interface IPingSessionMessage {
-    messageType: string
+  messageType: string
 }
 
 interface IPingSessionEchoMessage extends IPingSessionMessage {
-    echo?: string
+  echo?: string
 }
 
 export default class Monitoring {
-    static initWebSocketLatencyTest(hostname: string, accessToken: string) {
-        const url = `${hostname}/api/v1/pingresponder/some-id-for-this-device-or-user`;
+  private static _isConnected: boolean = false
 
-        const webSocket = new WebSocket(url, ['access_token', accessToken]);
+  static initWebSocketLatencyTest(hostname: string, accessToken: string) {
+    if (Monitoring._isConnected) return
 
-        let sendPing: NodeJS.Timer | null;
-        webSocket.addEventListener('open', () => {
-            console.log('Monitoring connection to the web socket opened')
-            sendPing = setInterval(() => {
-                const echoMessage: IPingSessionEchoMessage = {
-                    messageType: 'echo',
-                    // might want to use something finer-grained than milliseconds.
-                    echo: new Date().toISOString(),
-                }
-                webSocket.send(JSON.stringify(echoMessage))
-            }, 5000)
-        })
+    const url = `${hostname}/api/v1/pingresponder/some-id-for-this-device-or-user`
 
-        webSocket.addEventListener('message', (message: MessageEvent) => {
-            const nowDate = new Date()
-            const pingSessionMessage: IPingSessionEchoMessage = JSON.parse(message.data)
-            const thenString = pingSessionMessage.echo
-            if (!thenString) {
-                console.log(`Warning: Ping message lacked origination time.`)
-                return;
-            }
-            const thenTime = Date.parse(thenString)
-            const elapsedTime = nowDate.getTime() - thenTime
-            console.log(`RTT ${elapsedTime}ms`)
-        })
+    const webSocket = new WebSocket(url, ['access_token', accessToken])
 
-        webSocket.addEventListener('close', () => {
-            console.log('Monitoring connection to the web socket closed')
-            if (sendPing)
-                clearInterval(sendPing)
-        });
-    }
+    let sendPing: NodeJS.Timer | null
+    webSocket.addEventListener('open', () => {
+      console.log('Monitoring._isConnected', Monitoring._isConnected)
+
+      Monitoring._isConnected = true
+      sendPing = setInterval(() => {
+        const echoMessage: IPingSessionEchoMessage = {
+          messageType: 'echo',
+          // might want to use something finer-grained than milliseconds.
+          echo: new Date().toISOString()
+        }
+        webSocket.send(JSON.stringify(echoMessage))
+      }, 5000)
+    })
+
+    webSocket.addEventListener('message', (message: MessageEvent) => {
+      const nowDate = new Date()
+      const pingSessionMessage: IPingSessionEchoMessage = JSON.parse(message.data)
+      const thenString = pingSessionMessage.echo
+      if (!thenString) {
+        console.log(`Warning: Ping message lacked origination time.`)
+        return
+      }
+      const thenTime = Date.parse(thenString)
+      const elapsedTime = nowDate.getTime() - thenTime
+      console.log(`RTT ${elapsedTime}ms`)
+    })
+
+    webSocket.addEventListener('close', () => {
+      console.log('Monitoring connection to the web socket closed')
+      Monitoring._isConnected = false
+      if (sendPing) clearInterval(sendPing)
+    })
+  }
 }
